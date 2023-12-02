9add36333db96ac0767c2179bf5d3662ef9e127a
Okta Prianto <oktaprianto10@gmail.com>
2023-12-03 00:05:18 +0700

    Add tours.js

--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,8 +1,35 @@
 import express from "express";
-import { createTour } from "../controllers/tourControllers.js";
+import {
+    createTour,
+    deleteTour,
+    getSingleTour,
+    updateTour,
+    getAllTour,
+    getTourBySearch,
+    getFeaturedTour,
+    getTourCount,
+} from "../controllers/tourController.js";
 
 const router = express.Router();
 
-router.post('/', createTour);
+// create new tour
+router.post("/", createTour);
+
+// update new tour
+router.put("/:id", updateTour);
+
+// delete tour
+router.delete("/:id", deleteTour);
+
+// get single  tour
+router.get("/:id", getSingleTour);
+
+// get all  tour
+router.get("/", getAllTour);
+
+// get tour by search
+router.get("/search/getTourBySearch", getTourBySearch);
+router.get("/search/getFeaturedTour", getFeaturedTour);
+router.get("/search/getTourCount", getTourCount);
 
 export default router;
