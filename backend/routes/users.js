48a4a02b8b624efed4470953c67944c1d8b45d1e
Okta Prianto <oktaprianto10@gmail.com>
2023-12-03 23:51:50 +0700

    Add userController & users.js

---
+++ backend/routes/users.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+    deleteUser,
+    getSingleUser,
+    updateUser,
+    getAllUser,
+} from "../controllers/userController.js";
+const router = express.Router();
+
+
+import { verifyAdmin, verifyUser } from '../utils/verifyToken.js';
+
+// update new user
+router.put("/:id", verifyUser, updateUser);
+
+// delete user
+router.delete("/:id", verifyUser, deleteUser);
+
+// get single user
+router.get("/:id", verifyUser, getSingleUser);
+
+// get all user
+router.get("/", verifyAdmin, getAllUser);
+
+export default router;
